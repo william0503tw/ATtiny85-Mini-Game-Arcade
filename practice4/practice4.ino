b038e0f860961b2f6d6ceae5e1104b1630ea64fa
WilliamChiu <60286263+william0503tw@users.noreply.github.com>
2023-04-06 23:16:15 +0800

    first commit

---
+++ practice4/practice4.ino
@@ -0,0 +1,288 @@
+#include <EEPROM.h>
+#include "font6x8.h"
+#include <avr/sleep.h>
+#include <avr/interrupt.h> // needed for the additional interrupt
+
+#define DIGITAL_WRITE_HIGH(PORT) PORTB |= (1 << PORT)
+#define DIGITAL_WRITE_LOW(PORT) PORTB &= ~(1 << PORT)
+
+// Routines to set and clear bits (used in the sleep code)
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+
+// Defines for OLED output
+#define SSD1306XLED_H
+#define SSD1306_SCL   PORTB4  // SCL, Pin 4 on SSD1306 Board - for webbogles board
+#define SSD1306_SDA   PORTB3  // SDA, Pin 3 on SSD1306 Board - for webbogles board
+#define SSD1306_SA    0x78  // Slave address
+
+// Function prototype for SSD1306
+void ssd1306_init(void);
+void ssd1306_xfer_start(void);
+void ssd1306_xfer_stop(void);
+void ssd1306_send_byte(uint8_t byte);
+void ssd1306_send_command(uint8_t command);
+void ssd1306_send_data_start(void);
+void ssd1306_send_data_stop(void);
+void ssd1306_setpos(uint8_t x, uint8_t y);
+void ssd1306_fillscreen(uint8_t fill_Data);
+void ssd1306_char_f6x8(uint8_t x, uint8_t y, const char ch[]);
+void ssd1306_draw_bmp(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, uint8_t bitmap[]);
+//
+
+// Function prototype for Game
+void show_splashscreen();
+
+int x = 0 , y = 0 ;
+bool flag = 1 ;
+
+ISR(PCINT0_vect){ // PB0 pin button interrupt           
+}
+
+void setup() {
+  PCMSK = 0b00000001;   // pin change mask: listen to portb bit 1
+  GIMSK |= 0b00100000;  // enable PCINT interrupt 
+  sei();                // enable all interrupts
+
+  ssd1306_init();
+  ssd1306_clearScreen();
+  show_splashScreen();
+  delay(1000);
+  ssd1306_clearScreen();
+}
+
+void loop() {
+  ssd1306_char_f6x8(0, 7, "CCJ, Happy Birthday !");
+  for(int i = 0 ; i < 120 ; i += 10){
+    if(flag){
+      drawHeart2(i+10,y+1);
+      drawHeart(i+20,y+2);
+      drawHeart2(i+30,y+3);
+      drawHeart(i+40,y+4);
+      delay(150);
+      clearHeart(i+10,y+1);
+      clearHeart(i+20,y+2);
+      clearHeart(i+30,y+3);
+      clearHeart(i+40,y+4);
+      flag = !flag ;
+      }else{
+       drawHeart(i+10,y+1);
+       drawHeart2(i+20,y+2);
+       drawHeart(i+30,y+3);
+       drawHeart2(i+40,y+4);
+       delay(150);
+       clearHeart(i+10,y+1);
+       clearHeart(i+20,y+2);
+       clearHeart(i+30,y+3);
+       clearHeart(i+40,y+4);
+       flag = !flag ;
+       }
+  }
+}
+
+void drawHeart(int x, int y){
+  ssd1306_setpos(x,y);
+  ssd1306_send_data_start();
+  ssd1306_send_byte(B00011100);
+  ssd1306_send_byte(B00111110);
+  ssd1306_send_byte(B01111110);
+  ssd1306_send_byte(B11111100);
+  ssd1306_send_byte(B11111100);
+  ssd1306_send_byte(B01111110);
+  ssd1306_send_byte(B00111110);
+  ssd1306_send_byte(B00011100);
+  ssd1306_send_data_stop();
+  }
+
+void drawHeart2(int x , int y){
+  ssd1306_setpos(x,y);
+  ssd1306_send_data_start();
+  ssd1306_send_byte(B00011100);
+  ssd1306_send_byte(B00100010);
+  ssd1306_send_byte(B01000010);
+  ssd1306_send_byte(B10000100);
+  ssd1306_send_byte(B10000100);
+  ssd1306_send_byte(B01000010);
+  ssd1306_send_byte(B00100010);
+  ssd1306_send_byte(B00011100);
+  ssd1306_send_data_stop();
+}
+
+void clearHeart(int x, int y){
+  ssd1306_setpos(x,y);
+  ssd1306_send_data_start();
+  ssd1306_send_byte(B00000000);
+  ssd1306_send_byte(B00000000);
+  ssd1306_send_byte(B00000000);
+  ssd1306_send_byte(B00000000);
+  ssd1306_send_byte(B00000000);
+  ssd1306_send_byte(B00000000);
+  ssd1306_send_byte(B00000000);
+  ssd1306_send_byte(B00000000);
+  ssd1306_send_data_stop();
+}
+
+void show_splashScreen(){
+  ssd1306_char_f6x8(0, 1, "   ---------------  ");
+  ssd1306_char_f6x8(0, 2, "       N C T U      ");
+  ssd1306_char_f6x8(0, 4, "   M A K E R E A L  ");
+  ssd1306_char_f6x8(0, 5, "   ---------------  ");
+  ssd1306_char_f6x8(0, 7, "     William Chiu   ");
+  }
+
+void ssd1306_clearScreen(){
+  ssd1306_fillscreen(0x00);
+  }
+
+
+// SSD1306
+void ssd1306_init(void){
+  DDRB |= (1 << SSD1306_SDA); // Set port as output
+  DDRB |= (1 << SSD1306_SCL); // Set port as output
+
+  ssd1306_send_command(0xAE); // display off
+  ssd1306_send_command(0x00); // Set Memory Addressing Mode
+  ssd1306_send_command(0x10); // 00,Horizontal Addressing Mode;01,Vertical Addressing Mode;10,Page Addressing Mode (RESET);11,Invalid
+  ssd1306_send_command(0x40); // Set Page Start Address for Page Addressing Mode,0-7
+  ssd1306_send_command(0x81); // Set COM Output Scan Direction
+  ssd1306_send_command(0xCF); // ---set low column address
+  ssd1306_send_command(0xA1); // ---set high column address
+  ssd1306_send_command(0xC8); // --set start line address
+  ssd1306_send_command(0xA6); // --set contrast control register
+  ssd1306_send_command(0xA8);
+  ssd1306_send_command(0x3F); // --set segment re-map 0 to 127
+  ssd1306_send_command(0xD3); // --set normal display
+  ssd1306_send_command(0x00); // --set multiplex ratio(1 to 64)
+  ssd1306_send_command(0xD5); // 
+  ssd1306_send_command(0x80); // 0xa4,Output follows RAM content;0xa5,Output ignores RAM content
+  ssd1306_send_command(0xD9); // -set display offset
+  ssd1306_send_command(0xF1); // -not offset
+  ssd1306_send_command(0xDA); // --set display clock divide ratio/oscillator frequency
+  ssd1306_send_command(0x12); // --set divide ratio
+  ssd1306_send_command(0xDB); // --set pre-charge period
+  ssd1306_send_command(0x40); // 
+  ssd1306_send_command(0x20); // --set com pins hardware configuration
+  ssd1306_send_command(0x02);
+  ssd1306_send_command(0x8D); // --set vcomh
+  ssd1306_send_command(0x14); // 0x20,0.77xVcc
+  ssd1306_send_command(0xA4); // --set DC-DC enable
+  ssd1306_send_command(0xA6); // 
+  ssd1306_send_command(0xAF); // --turn on oled panel 
+}
+
+void ssd1306_xfer_start(void){
+  DIGITAL_WRITE_HIGH(SSD1306_SCL);  // Set to HIGH
+  DIGITAL_WRITE_HIGH(SSD1306_SDA);  // Set to HIGH
+  DIGITAL_WRITE_LOW(SSD1306_SDA);   // Set to LOW
+  DIGITAL_WRITE_LOW(SSD1306_SCL);   // Set to LOW
+}
+
+void ssd1306_xfer_stop(void){
+  DIGITAL_WRITE_LOW(SSD1306_SCL);   // Set to LOW
+  DIGITAL_WRITE_LOW(SSD1306_SDA);   // Set to LOW
+  DIGITAL_WRITE_HIGH(SSD1306_SCL);  // Set to HIGH
+  DIGITAL_WRITE_HIGH(SSD1306_SDA);  // Set to HIGH
+}
+
+void ssd1306_send_byte(uint8_t byte){
+  uint8_t i;
+  for(i=0; i<8; i++)
+  {
+    if((byte << i) & 0x80)
+      DIGITAL_WRITE_HIGH(SSD1306_SDA);
+    else
+      DIGITAL_WRITE_LOW(SSD1306_SDA);
+    
+    DIGITAL_WRITE_HIGH(SSD1306_SCL);
+    DIGITAL_WRITE_LOW(SSD1306_SCL);
+  }
+  DIGITAL_WRITE_HIGH(SSD1306_SDA);
+  DIGITAL_WRITE_HIGH(SSD1306_SCL);
+  DIGITAL_WRITE_LOW(SSD1306_SCL);
+}
+
+void ssd1306_send_command(uint8_t command){
+  ssd1306_xfer_start();
+  ssd1306_send_byte(SSD1306_SA);  // Slave address, SA0=0
+  ssd1306_send_byte(0x00);  // write command
+  ssd1306_send_byte(command);
+  ssd1306_xfer_stop();
+}
+
+void ssd1306_send_data_start(void){
+  ssd1306_xfer_start();
+  ssd1306_send_byte(SSD1306_SA);
+  ssd1306_send_byte(0x40);  //write data
+}
+
+void ssd1306_send_data_stop(void){
+  ssd1306_xfer_stop();
+}
+
+void ssd1306_setpos(uint8_t x, uint8_t y)
+{
+  if (y>7) return;
+  ssd1306_xfer_start();
+  ssd1306_send_byte(SSD1306_SA);  //Slave address,SA0=0
+  ssd1306_send_byte(0x00);  //write command
+
+  ssd1306_send_byte(0xb0+y);
+  ssd1306_send_byte(((x&0xf0)>>4)|0x10); // |0x10
+  ssd1306_send_byte((x&0x0f)|0x01); // |0x01
+
+  ssd1306_xfer_stop();
+}
+
+void ssd1306_fillscreen(uint8_t fill_Data){
+  uint8_t m,n;
+  for(m=0;m<8;m++)
+  {
+    ssd1306_send_command(0xb0+m); //page0-page1
+    ssd1306_send_command(0x00);   //low column start address
+    ssd1306_send_command(0x10);   //high column start address
+    ssd1306_send_data_start();
+    for(n=0;n<128;n++)
+    {
+      ssd1306_send_byte(fill_Data);
+    }
+    ssd1306_send_data_stop();
+  }
+}
+
+
+void ssd1306_char_f6x8(uint8_t x, uint8_t y, const char ch[]){
+  uint8_t c,i,j=0;
+  while(ch[j] != '\0')
+  {
+    c = ch[j] - 32;
+    if(x>126)
+    {
+      x=0;
+      y++;
+    }
+    ssd1306_setpos(x,y);
+    ssd1306_send_data_start();
+    for(i=0;i<6;i++)
+    {
+      ssd1306_send_byte(pgm_read_byte(&ssd1306xled_font6x8[c*6+i]));
+    }
+    ssd1306_send_data_stop();
+    x += 6;
+    j++;
+  }
+}
+
+void system_sleep() {
+  ssd1306_fillscreen(0x00);
+  ssd1306_send_command(0xAE);
+  cbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter OFF
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
+  sleep_enable();
+  sleep_mode();                        // System actually sleeps here
+  sleep_disable();                     // System continues execution here when watchdog timed out 
+  sbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter ON  
+  ssd1306_send_command(0xAF);
+}
+
+
+// For Game Logic
